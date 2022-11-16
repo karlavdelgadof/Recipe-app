8f58c76861698987f2ea105c93f50c22fdc4e072
karlavdelgadof <karlavdelgadof@gmail.com>
2022-11-16 16:15:38 -0500

    Generate app routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'recipes#index'
+  resources :recipe_foods
+  resources :inventory_foods
+  resources :inventories
+  resources :recipes
+  resources :foods
+  resources :users
+  # get "/public_recipes" to: "/views/layouts/public_recipes"
+  # resources :shopping_list
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

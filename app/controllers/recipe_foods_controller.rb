0a363db89eea60070143b1f74de61303ff8cb2a3
karlavdelgadof <karlavdelgadof@gmail.com>
2022-11-18 22:05:53 -0500

    Delete unused actions

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,14 +1,6 @@
 class RecipeFoodsController < ApplicationController
   before_action :set_recipe_food, only: %i[show edit update destroy]
 
-  # GET /recipe_foods or /recipe_foods.json
-  def index
-    @recipe_foods = RecipeFood.all
-  end
-
-  # GET /recipe_foods/1 or /recipe_foods/1.json
-  def show; end
-
   # GET /recipe_foods/new
   def new
     @recipe_food = RecipeFood.new
@@ -23,11 +15,9 @@ class RecipeFoodsController < ApplicationController
 
     respond_to do |format|
       if @recipe_food.save
-        format.html { redirect_to recipe_food_url(@recipe_food), notice: 'Recipe food was successfully created.' }
-        format.json { render :show, status: :created, location: @recipe_food }
+        format.html { redirect_to recipe_url(@recipe_food.recipe), notice: 'Recipe food was successfully created.' }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @recipe_food.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -51,7 +41,6 @@ class RecipeFoodsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to recipe_foods_url, notice: 'Recipe food was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -64,6 +53,6 @@ class RecipeFoodsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def recipe_food_params
-    params.require(:recipe_food).permit(:quantity)
+    params.require(:recipe_food).permit(:quantity, :recipe_id, :food_id)
   end
 end
